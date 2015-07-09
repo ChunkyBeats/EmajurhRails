4a65339676d8bb76d1c311baf9eec23a4350bc62
Cory Ribson & Jeff Lalic <student@epicodus.com>
2015-07-09 16:28:45 -0700

    Fixes current user check for showing delete-tag for images, adds front-page syling

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,6 @@ class Image < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title
 
-  has_attached_file :attachment, :styles => { :thumb => "250x250" }
+  has_attached_file :attachment, :styles => { :thumb => "250x250>", :medium => "800x800>" }
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
